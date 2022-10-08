f205693e66af0f9c21f2c6af4d386d92ea4da6ac
Fespis <nikita.zhuk00@gmail.com>
2022-10-08 16:45:05 +0300

    feat: add search input and buttons profile in header

--- shop/src/component/Header/HeaderComponent.jsx
+++ shop/src/component/Header/HeaderComponent.jsx
@@ -1,6 +1,18 @@
 import { Box } from "@mantine/core";
-import { AppShell, Navbar, Header, Image, Button, Burger } from "@mantine/core";
-import { IconDatabase } from "@tabler/icons";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Image,
+  Button,
+  Burger,
+  TextInput,
+  Group,
+  Badge,
+  Text,
+  Tooltip,
+} from "@mantine/core";
+import { IconSearch, IconUserCircle, IconBasket } from "@tabler/icons";
 import { useState } from "react";
 
 function HeaderComponent() {
@@ -8,33 +20,66 @@ function HeaderComponent() {
 
   return (
     <Header
-      height={170}
       p="xs"
       sx={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        textAlign: "center",
       }}
     >
-      <Image
-        width={200}
-        height={70}
-        fit="contain"
-        src="https://static.insales-cdn.com/files/1/3373/16379181/original/Component_22.png"
-        withPlaceholder
-      />
-      <Button
-        size="md"
-        variant="gradient"
-        onClick={() => setOpenedCatalog((open) => !open)}
-        leftIcon={
-          <Burger opened={openedCatalog} color="#ffffff" />
-        }
-        gradient={{ from: "teal", to: "lime", deg: 105 }}
-      >
-        Каталог
-      </Button>
+      <Box sx={{ width: 1620 }}>
+        <Group
+          spacing="xl"
+          sx={{ minWidth: "100%", justifyContent: "space-between" }}
+        >
+          <Group spacing="xl">
+            <Image
+              width={100}
+              height={70}
+              fit="contain"
+              src="https://static.insales-cdn.com/files/1/3373/16379181/original/Component_22.png"
+              withPlaceholder
+            />
+            <Badge
+              sx={{ width: 160, height: 42, cursor: "pointer" }}
+              radius="xs"
+              variant="gradient"
+              onClick={() => setOpenedCatalog((open) => !open)}
+              leftSection={<Burger opened={openedCatalog} color="#ffffff" />}
+              gradient={{ from: "teal", to: "lime", deg: 105 }}
+            >
+              <Text size="sm">Каталог</Text>
+            </Badge>
+            <TextInput
+              placeholder="Поиск"
+              radius="xs"
+              size="md"
+              sx={{ width: "35vw", maxWidth: 700, height: 42 }}
+              rightSection={<IconSearch />}
+            />
+          </Group>
+          <Group spacing="xs">
+            <Tooltip label="Профиль">
+              <Button
+                variant="default"
+                color="green"
+                sx={{ width: 70, height: 42 }}
+              >
+                <IconUserCircle />
+              </Button>
+            </Tooltip>
+            <Tooltip label="Корзина">
+              <Button
+                variant="default"
+                color="green"
+                sx={{ width: 70, height: 42 }}
+              >
+                <IconBasket />
+              </Button>
+            </Tooltip>
+          </Group>
+        </Group>
+      </Box>
     </Header>
   );
 }
