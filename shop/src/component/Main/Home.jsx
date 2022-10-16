6f2140bed061c47617863b8f84d06e72ef69c992
Fespis <nikita.zhuk00@gmail.com>
2022-10-16 14:57:39 +0300

    refactor: delete main banner in home page

--- shop/src/component/Main/Home.jsx
+++ shop/src/component/Main/Home.jsx
@@ -30,58 +30,6 @@ function Home() {
         justifyContent: "center",
       }}
     >
-      <Box>
-        <Grid columns={3}>
-          <Grid.Col
-            sx={{
-              backgroundColor: "#fff323",
-            }}
-            span={1}
-          >
-            1
-          </Grid.Col>
-          <Grid.Col
-            sx={{
-              backgroundColor: "#fff323",
-            }}
-            span={1}
-          >
-            2
-          </Grid.Col>
-          <Grid.Col
-            sx={{
-              backgroundColor: "#fff323",
-            }}
-            span={1}
-          >
-            3
-          </Grid.Col>
-          <Grid.Col
-            sx={{
-              backgroundColor: "#fff323",
-            }}
-            span={1}
-          >
-            4
-          </Grid.Col>
-          <Grid.Col
-            sx={{
-              backgroundColor: "#fff323",
-            }}
-            span={1}
-          >
-            5
-          </Grid.Col>
-          <Grid.Col
-            sx={{
-              backgroundColor: "#fff323",
-            }}
-            span={1}
-          >
-            6
-          </Grid.Col>
-        </Grid>
-      </Box>
     </Container>
   );
 }
