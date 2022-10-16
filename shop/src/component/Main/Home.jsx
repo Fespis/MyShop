9565bc3dc969162eedb5b4f2a5bd3dc1a3e94cff
Fespis <nikita.zhuk00@gmail.com>
2022-10-16 17:40:04 +0300

    feat: add new catalog in home page

--- shop/src/component/Main/Home.jsx
+++ shop/src/component/Main/Home.jsx
@@ -19,50 +19,68 @@ import {
   SimpleGrid,
   Skeleton,
   useMantineTheme,
+  ActionIcon,
 } from "@mantine/core";
+
+import { IconArrowNarrowRight, IconArrowNarrowLeft } from "@tabler/icons";
 
 function Home() {
   return (
     <Container my="md" sx={{ maxWidth: 1480 }}>
-      <SimpleGrid
-        cols={4}
-        spacing="xl"
-        breakpoints={[
-          { maxWidth: "xl", cols: 4 },
-          { maxWidth: "md", cols: 3 },
-          { maxWidth: "md", cols: 2 },
-          { maxWidth: "sm", cols: 2 },
-          { maxWidth: "xs", cols: 1 },
-        ]}
-      >
-        <Image
-          sx={{ cursor: "pointer" }}
-          radius="md"
-          src="https://static.insales-cdn.com/r/XwKQypt2ac0/rs:fill-down:547:342:1/q:100/plain/files/1/5894/16365318/original/Component_5-5-min.jpg"
-          alt="Random unsplash image"
-        />
+      <Stack spacing="xl">
+        <SimpleGrid
+          cols={4}
+          spacing="xl"
+          breakpoints={[
+            { maxWidth: "xl", cols: 4 },
+            { maxWidth: "md", cols: 3 },
+            { maxWidth: "md", cols: 2 },
+            { maxWidth: "sm", cols: 2 },
+            { maxWidth: "xs", cols: 1 },
+          ]}
+        >
+          <Image
+            sx={{ cursor: "pointer" }}
+            radius="md"
+            src="https://static.insales-cdn.com/r/XwKQypt2ac0/rs:fill-down:547:342:1/q:100/plain/files/1/5894/16365318/original/Component_5-5-min.jpg"
+            alt="Random unsplash image"
+          />
 
-        <Image
-          sx={{ cursor: "pointer" }}
-          radius="md"
-          src="https://static.insales-cdn.com/r/H7o2a6XDSMA/rs:fill-down:260:163:1/q:100/plain/files/1/5322/16364746/original/Component_5-3_143e82bedd6e26ac5f9ec8ec65aeb4cd.jpg"
-          alt="Random unsplash image"
-        />
+          <Image
+            sx={{ cursor: "pointer" }}
+            radius="md"
+            src="https://static.insales-cdn.com/r/H7o2a6XDSMA/rs:fill-down:260:163:1/q:100/plain/files/1/5322/16364746/original/Component_5-3_143e82bedd6e26ac5f9ec8ec65aeb4cd.jpg"
+            alt="Random unsplash image"
+          />
 
-        <Image
-          sx={{ cursor: "pointer" }}
-          radius="md"
-          src="https://static.insales-cdn.com/r/ObtSC0YvoZA/rs:fill-down:260:163:1/q:100/plain/files/1/5985/17307489/original/Component_5-4-min_4646b7946c16baf088d50f67227c015b.jpg"
-          alt="Random unsplash image"
-        />
+          <Image
+            sx={{ cursor: "pointer" }}
+            radius="md"
+            src="https://static.insales-cdn.com/r/ObtSC0YvoZA/rs:fill-down:260:163:1/q:100/plain/files/1/5985/17307489/original/Component_5-4-min_4646b7946c16baf088d50f67227c015b.jpg"
+            alt="Random unsplash image"
+          />
 
-        <Image
-          sx={{ cursor: "pointer" }}
-          radius="md"
-          src="https://static.insales-cdn.com/r/nHITxemcuwY/rs:fill-down:260:163:1/q:100/plain/files/1/5982/17307486/original/2.jpg"
-          alt="Random unsplash image"
-        />
-      </SimpleGrid>
+          <Image
+            sx={{ cursor: "pointer" }}
+            radius="md"
+            src="https://static.insales-cdn.com/r/nHITxemcuwY/rs:fill-down:260:163:1/q:100/plain/files/1/5982/17307486/original/2.jpg"
+            alt="Random unsplash image"
+          />
+        </SimpleGrid>
+        <Group position="apart">
+          <Text variant="outline" sx={{ fontWeight: 500, fontSize: "1.2rem" }}>
+            Смартфоны и планшеты
+          </Text>
+          <Group>
+            <ActionIcon variant="subtle">
+              <IconArrowNarrowLeft />
+            </ActionIcon>
+            <ActionIcon variant="subtle">
+              <IconArrowNarrowRight />
+            </ActionIcon>
+          </Group>
+        </Group>
+      </Stack>
     </Container>
   );
 }
