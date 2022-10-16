d1cab08bef3f33cbcd108fcc18cff23a4d3f35da
Fespis <nikita.zhuk00@gmail.com>
2022-10-16 16:12:02 +0300

    feat: add simple grid in home page

--- shop/src/component/Main/Home.jsx
+++ shop/src/component/Main/Home.jsx
@@ -16,20 +16,92 @@ import {
   Stack,
   Grid,
   Aside,
+  SimpleGrid,
+  Skeleton,
+  useMantineTheme,
 } from "@mantine/core";
 
 function Home() {
+  const PRIMARY_COL_HEIGHT = 300;
+
+  const theme = useMantineTheme();
+  console.log(theme);
+  const SECONDARY_COL_HEIGHT = PRIMARY_COL_HEIGHT / 2 - theme.spacing.md / 2;
+
   return (
-    <Container
-      p="xs"
-      size={1640}
-      px={0}
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <Container my="md" sx={{ maxWidth: 1620 }}>
+      <SimpleGrid
+        cols={4}
+        spacing="xl"
+        breakpoints={[
+          { maxWidth: "xl", cols: 4 },
+          { maxWidth: "md", cols: 3 },
+          { maxWidth: "md", cols: 2 },
+          { maxWidth: "sm", cols: 2 },
+          { maxWidth: "xs", cols: 1 },
+        ]}
+      >
+        <Image
+          height={PRIMARY_COL_HEIGHT}
+          radius="md"
+          alt="Random unsplash image"
+        />
+
+        <Grid gutter="md">
+          <Grid.Col>
+            <Image
+              height={SECONDARY_COL_HEIGHT}
+              radius="md"
+              animate={false}
+              alt="Random unsplash image"
+            />
+          </Grid.Col>
+          <Grid.Col>
+            <Image
+              height={SECONDARY_COL_HEIGHT}
+              radius="md"
+              animate={false}
+              alt="Random unsplash image"
+            />
+          </Grid.Col>
+        </Grid>
+        <Grid gutter="md">
+          <Grid.Col>
+            <Image
+              height={SECONDARY_COL_HEIGHT}
+              radius="md"
+              animate={false}
+              alt="Random unsplash image"
+            />
+          </Grid.Col>
+          <Grid.Col>
+            <Image
+              height={SECONDARY_COL_HEIGHT}
+              radius="md"
+              animate={false}
+              alt="Random unsplash image"
+            />
+          </Grid.Col>
+        </Grid>
+        <Grid gutter="md">
+          <Grid.Col>
+            <Image
+              height={SECONDARY_COL_HEIGHT}
+              radius="md"
+              animate={false}
+              alt="Random unsplash image"
+            />
+          </Grid.Col>
+          <Grid.Col>
+            <Image
+              height={SECONDARY_COL_HEIGHT}
+              radius="md"
+              animate={false}
+              alt="Random unsplash image"
+            />
+          </Grid.Col>
+        </Grid>
+      </SimpleGrid>
     </Container>
   );
 }
