75bdd9ecb0b47b10cff7aba0040587edd37ace44
Fespis <nikita.zhuk00@gmail.com>
2022-11-28 22:12:20 +0300

    refactor:  change showcase home page

--- shop/src/component/Carousel/CarouselShowcases.jsx
+++ shop/src/component/Carousel/CarouselShowcases.jsx
@@ -26,26 +26,23 @@ const useStyles = createStyles((theme) => ({
   //   backgroundSize: "cover",
   //   backgroundPosition: "center",
   // },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontWeight: 400,
-    color: theme.black,
-    fontSize: 16,
-    marginTop: theme.spacing.xs,
-  },
-
-  price: {
-    color: theme.black,
-    opacity: 0.7,
-    fontWeight: 700,
-    fontSize: 24,
-    textTransform: "uppercase",
-  },
-
-  maxWidthClass: {
-    width: "100%",
-  },
+  // title: {
+  //   fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  //   fontWeight: 400,
+  //   color: theme.black,
+  //   fontSize: 16,
+  //   marginTop: theme.spacing.xs,
+  // },
+  // price: {
+  //   color: theme.black,
+  //   opacity: 0.7,
+  //   fontWeight: 700,
+  //   fontSize: 24,
+  //   textTransform: "uppercase",
+  // },
+  // maxWidthClass: {
+  //   width: "100%",
+  // },
 }));
 
 function Showcases({ image, title, category, price }) {
@@ -55,7 +52,7 @@ function Showcases({ image, title, category, price }) {
 
   return (
     <Paper shadow="md" p="xl" radius="xs" className={classes.card}>
-      <Group position="left" spacing="xs" className={classes.maxWidthClass}>
+      <Group position="apart" spacing="xs" className={classes.maxWidthClass}>
         <Image
           width={100}
           height={125}
@@ -104,13 +101,13 @@ const slideData = [
       },
       {
         image:
-          "https://static.insales-cdn.com/r/893xLRXMbrU/rs:fit:140:0:1/plain/images/products/1/3803/414199515/compact_sm_215_galaxya21s_black_front_raw.jpg@webp",
+          "https://static.insales-cdn.com/r/96bGYLn8AE8/rs:fit:400:0:1/q:100/plain/images/products/1/3723/414199435/large_12__1_.jpg@webp",
         title: "Смартфон Siaomi Nokia F1",
         price: "899",
       },
       {
         image:
-          "https://static.insales-cdn.com/r/893xLRXMbrU/rs:fit:140:0:1/plain/images/products/1/3803/414199515/compact_sm_215_galaxya21s_black_front_raw.jpg@webp",
+          "https://static.insales-cdn.com/r/-Vz4S3vnVow/rs:fit:400:0:1/q:100/plain/images/products/1/3639/414199351/large_sm_a515_galaxya51_white_front_raw__1_.jpg@webp",
         title: "Смартфон Ksiaomi WTF",
         price: "899",
       },
@@ -121,19 +118,19 @@ const slideData = [
     goods: [
       {
         image:
-          "https://static.insales-cdn.com/r/-5WEpnQJI3w/rs:fit:140:0:1/plain/images/products/1/1412/493127044/compact_1.webp",
+          "https://static.insales-cdn.com/r/pyq0Flm8VOY/rs:fit:140:0:1/plain/images/products/1/7819/493133451/compact_1.webp",
         title: "Ноутбуки ASUS Giotaro",
         price: "1500",
       },
       {
         image:
-          "https://static.insales-cdn.com/r/-5WEpnQJI3w/rs:fit:140:0:1/plain/images/products/1/1412/493127044/compact_1.webp",
+          "https://static.insales-cdn.com/r/tFDzh6qF2Ug/rs:fit:400:0:1/q:100/plain/images/products/1/6391/493140215/large_1.webp",
         title: "Ноутбуки ASUS Dio",
         price: "1975",
       },
       {
         image:
-          "https://static.insales-cdn.com/r/-5WEpnQJI3w/rs:fit:140:0:1/plain/images/products/1/1412/493127044/compact_1.webp",
+          "https://static.insales-cdn.com/r/gLnPG2Vv1YY/rs:fit:400:0:1/q:100/plain/images/products/1/2716/493152924/large_1.webp",
         title: "Ноутбуки ASUS Rog Fight",
         price: "2130",
       },
@@ -144,19 +141,19 @@ const slideData = [
     goods: [
       {
         image:
-          "https://static.insales-cdn.com/r/yagq3Rups_E/rs:fit:240:240:1/plain/images/collections/1/293/81690917/medium_05.jpg@webp",
+          "https://static.insales-cdn.com/r/qtnOWPh5oaM/rs:fit:400:0:1/q:100/plain/images/products/1/1399/492660087/large_1.webp",
         title: "Телевизор Zekka Adia",
         price: "720",
       },
       {
         image:
-          "https://static.insales-cdn.com/r/yagq3Rups_E/rs:fit:240:240:1/plain/images/collections/1/293/81690917/medium_05.jpg@webp",
+          "https://static.insales-cdn.com/r/X8L6Psvoxjo/rs:fit:400:0:1/q:100/plain/images/products/1/5281/492647585/large_1.webp",
         title: "Телевизор Porshe Caien",
         price: "1860",
       },
       {
         image:
-          "https://static.insales-cdn.com/r/yagq3Rups_E/rs:fit:240:240:1/plain/images/collections/1/293/81690917/medium_05.jpg@webp",
+          "https://static.insales-cdn.com/r/TTmNoflBLbI/rs:fit:400:0:1/q:100/plain/images/products/1/5514/492664202/large_1.webp",
         title: "Телевизор Samsung Zero",
         price: "1250",
       },
@@ -170,7 +167,7 @@ export function CarouselShowcases() {
   return (
     <>
       <Carousel
-        slideSize="33.33333%"
+        slideSize={1480 / 3 - 5}
         slidesToScroll={mobile ? 1 : 0}
         slideGap="xs"
         withControls={mobile ? true : false}
