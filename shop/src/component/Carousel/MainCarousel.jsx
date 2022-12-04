36ca2c9b059730871d267962f26ede5cd28893db
Fespis <nikita.zhuk00@gmail.com>
2022-12-04 16:04:31 +0300

    feat: add images for main carousel home page

--- shop/src/component/Carousel/MainCarousel.jsx
+++ shop/src/component/Carousel/MainCarousel.jsx
@@ -64,23 +64,23 @@ const data = [
   {
     id: 1,
     mobileImage:
+      "https://sun9-71.userapi.com/impg/6ynr6cFLtEqMSEWSoy2fxq3VrpynXE1EjtH3Pg/6e2BViT9VxQ.jpg?size=480x320&quality=95&sign=8ae57e7a81a8706f2b4c43df1a3786b7&type=album",
+    image:
+      "https://img.5element.by/upload/rk/778/77859834259a52ce699593faf9b4dea8.jpg",
+  },
+  {
+    id: 2,
+    mobileImage:
       "https://img.5element.by/upload/rk/8a9/8a95cba4a0f959c85d7c3f15ea91c4ff.jpg",
     image:
       "https://img.5element.by/upload/rk/698/6989dad10df67778ba4a2ce64a481671.jpg",
   },
   {
-    id: 2,
-    mobileImage:
-      "https://img.5element.by/upload/rk/778/77859834259a52ce699593faf9b4dea8.jpg",
-    image:
-      "https://img.5element.by/upload/rk/778/77859834259a52ce699593faf9b4dea8.jpg",
-  },
-  {
     id: 3,
     mobileImage:
-      "https://img.5element.by/upload/rk/5da/5da7aee4d19a64d9758fc91ddd8ac87e.jpg",
+      "https://sun9-29.userapi.com/impg/wgD5MwdztxoemqYswgdc1BohO8RPlNldRHHecA/rhPB6yMlkSk.jpg?size=480x320&quality=95&sign=b803806eb6bfc45eb3a33573ac50ed59&type=album",
     image:
-      "https://img.5element.by/upload/rk/5da/5da7aee4d19a64d9758fc91ddd8ac87e.jpg",
+      "https://img.5element.by/upload/rk/5b2/5b2d403df9ee82b0c18703e82311a05a.jpg",
   },
 ];
 
