5387ad8779f229d50dfc32e4a3c2b777fe4df968
Fespis <nikita.zhuk00@gmail.com>
2022-12-05 20:37:40 +0300

    feat: add cat developer notFound page

--- shop/src/component/Main/NotFound.jsx
+++ shop/src/component/Main/NotFound.jsx
@@ -1,8 +1,39 @@
 function NotFound() {
   return (
-    <>
-      <div>NotFound</div>
-    </>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div class="container">
+        <div class="kitty">
+          <div class="face">
+            <div class="ear"></div>
+            <div class="eye"></div>
+            <div class="mouth">
+              <div class="nose"></div>
+              <div class="whisker-1"></div>
+              <div class="whisker-2"></div>
+            </div>
+            <div class="body">
+              <div class="hand">
+                <div class="hand-l"></div>
+                <div class="hand-r"></div>
+              </div>
+            </div>
+            <div class="tail"></div>
+          </div>
+          <div class="laptop">
+            <div class="logo"></div>
+          </div>
+        </div>
+      </div>
+      <p>Раздел в разработке!</p>
+      <p>Прошу не мешать котику!</p>
+    </div>
   );
 }
 
