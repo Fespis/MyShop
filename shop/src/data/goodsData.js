dfd9d696a8fbd3dcdcea71d08325ea9cd008d776
Fespis <nikita.zhuk00@gmail.com>
2022-12-08 01:52:21 +0300

    feat: add data base goods

---
+++ shop/src/data/goodsData.js
@@ -0,0 +1,318 @@
+const goodsData = [
+  {
+    category: "Смартфоны",
+    goods: [
+      {
+        image:
+          "https://static.insales-cdn.com/r/893xLRXMbrU/rs:fit:140:0:1/plain/images/products/1/3803/414199515/compact_sm_215_galaxya21s_black_front_raw.jpg@webp",
+        title: "Смартфон Samsung Galaxy A12 64GB Black (SM-A125F)",
+        price: "1200",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/96bGYLn8AE8/rs:fit:400:0:1/q:100/plain/images/products/1/3723/414199435/large_12__1_.jpg@webp",
+        title: "Смартфон Samsung Galaxy S21 128GB (SM-G991B)",
+        price: "1300",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/-Vz4S3vnVow/rs:fit:400:0:1/q:100/plain/images/products/1/3639/414199351/large_sm_a515_galaxya51_white_front_raw__1_.jpg@webp",
+        title: "Смартфон Samsung Galaxy S21 256GB (SM-G991B)",
+        price: "560",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/WVlQE-v_hho/rs:fit:400:0:1/q:100/plain/images/products/1/3803/414199515/large_sm_215_galaxya21s_black_front_raw.jpg@webp",
+        title: "Смартфон Samsung Galaxy A12 32GB Black (SM-A125F)",
+        price: "899",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/WVlQE-v_hho/rs:fit:400:0:1/q:100/plain/images/products/1/3803/414199515/large_sm_215_galaxya21s_black_front_raw.jpg@webp",
+        title: "Смартфон Samsung Galaxy S4 128GB (SM-G923B)",
+        price: "1100",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/UeuMqKFKWFk/rs:fit:400:0:1/q:100/plain/images/products/1/4034/414199746/large_ae-galaxy-m31-m315-sm-m315fzkvxsg-frontblack-214808359.jpg@webp",
+        title: "Смартфон Samsung Galaxy S8 128GB (SM-G954B)",
+        price: "1169",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/UeuMqKFKWFk/rs:fit:400:0:1/q:100/plain/images/products/1/4034/414199746/large_ae-galaxy-m31-m315-sm-m315fzkvxsg-frontblack-214808359.jpg@webp",
+        title: "Смартфон Xiaomi Mi 10T 8+128GB Black",
+        price: "2500",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/cYIyc4aNUvU/rs:fit:400:0:1/q:100/plain/images/products/1/3355/414199067/large_sm_a715_galaxya71_black_front_raw__1_.jpg@webp",
+        title: "Смартфон Nokia 3.4",
+        price: "659",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/96bGYLn8AE8/rs:fit:400:0:1/q:100/plain/images/products/1/3723/414199435/large_12__1_.jpg@webp",
+        title: "Смартфон Xiaomi Redmi 9",
+        price: "1199",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/cYIyc4aNUvU/rs:fit:400:0:1/q:100/plain/images/products/1/3355/414199067/large_sm_a715_galaxya71_black_front_raw__1_.jpg@webp",
+        title: "Смартфон Samsung Galaxy A31",
+        price: "1439",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/UeuMqKFKWFk/rs:fit:400:0:1/q:100/plain/images/products/1/4034/414199746/large_ae-galaxy-m31-m315-sm-m315fzkvxsg-frontblack-214808359.jpg@webp",
+        title: "Смартфон Samsung Galaxy A51",
+        price: "1650",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/96bGYLn8AE8/rs:fit:400:0:1/q:100/plain/images/products/1/3723/414199435/large_12__1_.jpg@webp",
+        title: "Смартфон Samsung Galaxy D23",
+        price: "2080",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/-Vz4S3vnVow/rs:fit:400:0:1/q:100/plain/images/products/1/3639/414199351/large_sm_a515_galaxya51_white_front_raw__1_.jpg@webp",
+        title: "Смартфон Samsung Galaxy AAA",
+        price: "4540",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/96bGYLn8AE8/rs:fit:400:0:1/q:100/plain/images/products/1/3723/414199435/large_12__1_.jpg@webp",
+        title: "Смартфон Xiaomi Redmi A+",
+        price: "1460",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/UeuMqKFKWFk/rs:fit:400:0:1/q:100/plain/images/products/1/4034/414199746/large_ae-galaxy-m31-m315-sm-m315fzkvxsg-frontblack-214808359.jpg@webp",
+        title: "Смартфон Xiaomi Mi 10T 8+128GB White",
+        price: "2400",
+      },
+    ],
+  },
+  {
+    category: "Ноутбуки",
+    goods: [
+      {
+        image:
+          "https://static.insales-cdn.com/r/pyq0Flm8VOY/rs:fit:140:0:1/plain/images/products/1/7819/493133451/compact_1.webp",
+        title: `14.1" Ноутбук Haier A1410EM, Intel Celeron N4000 (1.1 ГГц), RAM 4 ГБ, eMMC 64 ГБ, Intel HD Graphics 600, Windows 10 Home, (JM02VHE08RU)`,
+        price: "1500",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/tFDzh6qF2Ug/rs:fit:400:0:1/q:100/plain/images/products/1/6391/493140215/large_1.webp",
+        title: `14" Ноутбук ASUS VivoBook X415JA-EK220T, Intel Core i5-1035G1 (1.0 ГГц)`,
+        price: "1975",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/gLnPG2Vv1YY/rs:fit:400:0:1/q:100/plain/images/products/1/2716/493152924/large_1.webp",
+        title: `16.1" Ноутбук Honor MagicBook Pro, AMD Ryzen 5 4600H (3.0 ГГц)`,
+        price: "2130",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/DbWT_ZDbqAo/rs:fit:1000:0:1/q:100/plain/images/products/1/1412/493127044/1.webp",
+        title: `14" Ноутбук ASUS E406NA-BV014T, RAM 4 ГБ, eMMC 128 ГБ`,
+        price: "2250",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/9PRz-3F0aYE/rs:fit:1000:0:1/q:100/plain/images/products/1/7819/493133451/1.webp",
+        title: `14.1" Ноутбук Haier A1410EM, Intel Celeron N4000 (1.1 ГГц), RAM 4 ГБ, eMMC 64 ГБ, Intel HD Graphics 600, Windows 10 Home, (JM02VHE08RU)`,
+        price: "1820",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/Ll14eUu24eU/rs:fit:1000:0:1/q:100/plain/images/products/1/2109/493135933/1.webp",
+        title: `15.6" Ноутбук Haier U1520EM, Intel Celeron N4020 (1.1 ГГц), RAM 4 ГБ, eMMC 64 ГБ, Intel HD Graphics 600, Windows 10 Home, (JM02VSE09RU)`,
+        price: "2030",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/K-OZeECmUVI/rs:fit:1000:0:1/q:100/plain/images/products/1/3900/493137724/1.webp",
+        title: `14" Ноутбук ASUS VivoBook X415JA-EK220T, Intel Core i5-1035G1 (1.0 ГГц)`,
+        price: "4600",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/9HlVBadj-XM/rs:fit:1000:0:1/q:100/plain/images/products/1/6391/493140215/1.webp",
+        title: `14.1" Ноутбук Digma EVE 14 C411, Intel Celeron N3350 (1.1 ГГц)`,
+        price: "1760",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/nuR7oAUDiwo/rs:fit:1000:0:1/q:100/plain/images/products/1/7850/493141674/1.webp",
+        title: `14" Ноутбук Honor MagicBook, AMD Ryzen 5 3600H (3.0 ГГц)`,
+        price: "1500",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/fHfEvJjw82Y/rs:fit:1000:0:1/q:100/plain/images/products/1/2679/493144695/1.webp",
+        title: `15.6" Ноутбук HP 15-db1240ur, AMD Ryzen 3 3200U (2.6 ГГц)`,
+        price: "3200",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/mgfK4XWA9B4/rs:fit:1000:0:1/q:100/plain/images/products/1/5348/493147364/1.webp",
+        title: `17.3" Ноутбук Lenovo IdP3 17ITL6, Intel Core i5-1135G7`,
+        price: "5300",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/Dq3L-czxAJQ/rs:fit:1000:0:1/q:100/plain/images/products/1/6045/493148061/1.webp",
+        title: `13.3" Ноутбук Apple MacBook Air M1`,
+        price: "10250",
+      },
+    ],
+  },
+  {
+    category: "Видеокамеры",
+    goods: [
+      {
+        image:
+          "https://static.insales-cdn.com/r/B2O8ckK-FJQ/rs:fit:400:0:1/q:100/plain/images/products/1/5041/494138289/large_1.webp",
+        title: "Panasonic HC-V270, Black цифровая видеокамера",
+        price: "720",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/JiIZIJUhBt4/rs:fit:400:0:1/q:100/plain/images/products/1/2408/494135656/large_1.webp",
+        title: "Видеокамера Panasonic HC-V770, Black",
+        price: "18600",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/rXoo7sJuehk/rs:fit:400:0:1/q:100/plain/images/products/1/7299/494140547/large_1.webp",
+        title:
+          "Экшн-камера GoPro Экшн-камера GoPro Hero 9 Black Edition CHDHX-901-RW, черный",
+        price: "1250",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/SSCiSY5CgUI/rs:fit:1000:0:1/q:100/plain/images/products/1/5212/494122076/1.webp",
+        title: "Монопод-трипод 3 в 1 c Led подсветкой",
+        price: "870",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/tYhGvLaxAbQ/rs:fit:1000:0:1/q:100/plain/images/products/1/6075/494122939/1.webp",
+        title:
+          "Фотоаппарат моментальной печати Fujifilm Instax MINI 11 нежная лаванда",
+        price: "7000",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/iDGrevIAAwc/rs:fit:1000:0:1/q:100/plain/images/products/1/8187/494125051/1.webp",
+        title: "Фотоаппарат мгновенной печати Fujifilm Instax SQ1, белый",
+        price: "11900",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/Exi0hdKYD_A/rs:fit:1000:0:1/q:100/plain/images/products/1/2122/494127178/1.webp",
+        title: "Детская видеокамера, синий",
+        price: "960",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/DMGekY-MTuY/rs:fit:1000:0:1/q:100/plain/images/products/1/4206/494137454/1.webp",
+        title: "Домашняя Wi-Fi камера,видеокамера в часах",
+        price: "5900",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/ggM0gS1hfIA/rs:fit:1000:0:1/q:100/plain/images/products/1/6154/494139402/1.webp",
+        title: "Экшн-камера SJCAM SJ8 Pro",
+        price: "13700",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/cvXRXidEJ_E/rs:fit:1000:0:1/q:100/plain/images/products/1/8162/494141410/1.webp",
+        title: "Экшн-камера X-Try Экшн-камера X-Try EMR ХТС196",
+        price: "4600",
+      },
+    ],
+  },
+  {
+    category: "Телевизоры",
+    goods: [
+      {
+        image:
+          "https://static.insales-cdn.com/r/qtnOWPh5oaM/rs:fit:400:0:1/q:100/plain/images/products/1/1399/492660087/large_1.webp",
+        title: `Ultra HD Телевизор Philips 50PUS7506/60 50"`,
+        price: "720",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/X8L6Psvoxjo/rs:fit:400:0:1/q:100/plain/images/products/1/5281/492647585/large_1.webp",
+        title: `4K UHD Телевизор Samsung UE70AU7100UXRU 70"`,
+        price: "1860",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/TTmNoflBLbI/rs:fit:400:0:1/q:100/plain/images/products/1/5514/492664202/large_1.webp",
+        title: `HD Телевизор ECON Безрамочный SMART 39" Linux с цифровым тюнером DVB-T2/DVB-C, модулем WI-FI и USB медиаплеером с поддержкой MKV видео 39"`,
+        price: "1250",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/33jjFfBgEdI/rs:fit:1000:0:1/q:100/plain/images/products/1/1568/492643872/1.webp",
+        title: `HD Телевизор Philips SMART Безрамочный`,
+        price: "3200",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/33jjFfBgEdI/rs:fit:1000:0:1/q:100/plain/images/products/1/1568/492643872/1.webp",
+        title: `HD Телевизор Xiaomi MI TV 32 P1 32"`,
+        price: "7200",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/t7F99LOgvjE/rs:fit:1000:0:1/q:100/plain/images/products/1/7471/492649775/1.webp",
+        title: `4K UHD Телевизор Xiaomi TV 4S 65"`,
+        price: "5400",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/dNgkZpzruM8/rs:fit:1000:0:1/q:100/plain/images/products/1/5574/492656070/1.webp",
+        title: `120K+ UHD Телевизор Samsung UE70AU7100UXRU 70"`,
+        price: "7300",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/kXFd_8U4AYg/rs:fit:1000:0:1/q:100/plain/images/products/1/6682/492657178/1.webp",
+        title: `Full HD Телевизор POLARLINE 42PL11TC 42"`,
+        price: "7300",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/eCFhoRsxk9k/rs:fit:1000:0:1/q:100/plain/images/products/1/7816/492658312/1.webp",
+        title: `Full HD Телевизор Supra LED 43"FULL HD с цифровым тюнером DVB-T2, мультимедиа плеером и режимом Отель 43"`,
+        price: "17600",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/J1WdjsFbqH0/rs:fit:1000:0:1/q:100/plain/images/products/1/325/492659013/1.webp",
+        title: `4K UHD Телевизор Samsung UE55AU9000UXRU 55"`,
+        price: "5600",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/cm8_le5dpTM/rs:fit:1000:0:1/q:100/plain/images/products/1/1399/492660087/1.webp",
+        title: `HD Телевизор BQ 32S01B 31.5"`,
+        price: "1400",
+      },
+      {
+        image:
+          "https://static.insales-cdn.com/r/ICjB27Bhczw/rs:fit:1000:0:1/q:100/plain/images/products/1/2223/492660911/1.webp",
+        title: `HD Телевизор Polar P20L32T2C 20"`,
+        price: "10200",
+      },
+    ],
+  },
+];
