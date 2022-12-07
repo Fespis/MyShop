3a263e3341a4a784c05d340ad2cde549df741ff2
Fespis <nikita.zhuk00@gmail.com>
2022-12-07 23:22:49 +0300

    rafactor: delete styles footer

--- shop/src/component/Footer/FooterComponentA.jsx
+++ shop/src/component/Footer/FooterComponentA.jsx
@@ -67,7 +67,6 @@ const useStyles = createStyles((theme) => ({
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      gap: 15,
     },
   },
 
@@ -126,41 +125,7 @@ const useStyles = createStyles((theme) => ({
 
   root: {
     width: "inherit",
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[6]
-        : theme.colors.gray[0],
     borderRadius: theme.radius.sm,
-  },
-
-  item: {
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[6]
-        : theme.colors.gray[0],
-    border: "1px solid transparent",
-    position: "relative",
-    zIndex: 0,
-    transition: "transform 150ms ease",
-
-    "&[data-active]": {
-      transform: "scale(1.03)",
-      backgroundColor:
-        theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-      boxShadow: theme.shadows.md,
-      borderColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[4]
-          : theme.colors.gray[2],
-      borderRadius: theme.radius.md,
-      zIndex: 1,
-    },
-  },
-
-  chevron: {
-    "&[data-rotate]": {
-      transform: "rotate(-90deg)",
-    },
   },
 }));
 
